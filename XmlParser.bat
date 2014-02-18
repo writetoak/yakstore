9dfd8502c649eab108396568089794200781b63d
writetoak <amit.khurana@live.com>
2014-02-18 13:46:48 +0530

    corrected classpath as now package name is yak-shop

--- XmlParser.bat
+++ XmlParser.bat
@@ -1,3 +1,3 @@
-set classpath=.\target\yakstore-1.0.0-BUILD-SNAPSHOT\WEB-INF\lib\spring-core-4.0.1.RELEASE.jar;.\target\yakstore-1.0.0-BUILD-SNAPSHOT\WEB-INF\lib\spring-context-4.0.1.RELEASE.jar;.\target\yakstore-1.0.0-BUILD-SNAPSHOT\WEB-INF\lib\spring-beans-4.0.1.RELEASE.jar;.\target\yakstore-1.0.0-BUILD-SNAPSHOT\WEB-INF\lib\spring-oxm-4.0.1.RELEASE.jar;.\target\yakstore-1.0.0-BUILD-SNAPSHOT\WEB-INF\lib\log4j-1.2.15.jar;.\commons-logging-1.1.3.jar;.\target\yakstore-1.0.0-BUILD-SNAPSHOT\WEB-INF\classes;
+set classpath=.\target\yak-shop\WEB-INF\lib\spring-core-4.0.1.RELEASE.jar;.\target\yak-shop\WEB-INF\lib\spring-context-4.0.1.RELEASE.jar;.\target\yak-shop\WEB-INF\lib\spring-beans-4.0.1.RELEASE.jar;.\target\yak-shop\WEB-INF\lib\spring-oxm-4.0.1.RELEASE.jar;.\target\yak-shop\WEB-INF\lib\log4j-1.2.15.jar;.\commons-logging-1.1.3.jar;.\target\yak-shop\WEB-INF\classes;
 java com.app.yakstore.util.XmlParser C:\herd.xml 14
 
